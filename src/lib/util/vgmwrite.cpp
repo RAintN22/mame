2a29253cddca9b9b9ba6836fa1efa68959d5ef8f
RAintN22 <105845701+RAintN22@users.noreply.github.com>
2025-05-30 18:47:48 +0800

    fix command for bsmt2000

--- src/lib/util/vgmwrite.cpp
+++ src/lib/util/vgmwrite.cpp
@@ -1982,14 +1982,14 @@ void VGMDeviceLog::Write(uint8_t port, uint16_t r, uint8_t v)
 		switch (port)
 		{
 			case 0: // Register write
-				wrtCmd.Data[0x00] = 0x46;
+				wrtCmd.Data[0x00] = 0xC9;
 				wrtCmd.Data[0x01] = (v & 0x7F) | (_chipType & 0x80);
 				wrtCmd.Data[0x02] = (r & 0xFF00) >> 8; // Data MSB
 				wrtCmd.Data[0x03] = (r & 0x00FF) >> 0; // Data LSB
 				wrtCmd.CmdLen = 0x04;
 				break;
 			case 1: // Mode change
-				wrtCmd.Data[0x00] = 0x47;
+				wrtCmd.Data[0x00] = 0x32;
 				wrtCmd.Data[0x01] = (r & 0x7F) | (_chipType & 0x80);
 				wrtCmd.CmdLen = 0x02;
 				break;
